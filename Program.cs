4374cffa47cf5859b96c1ea8034d6390902f3d55
Chloe Vandeworp <339639510@GoTVDSB.ca>
2025-02-12 13:44:17 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Topic_4___Random_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
